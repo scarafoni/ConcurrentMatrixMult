dc01a43b511c734165e4667f9122d9b607716ff4
Dan Scarafoni <dscarafo@u.rochester.edu>
2014-08-08 23:17:32 -0400

    Update Main.hs
    Array test draft

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,10 @@
 module Main where
 import System.Environment
+import Data.Array.IO
 
 main :: IO ()
 main = do
     args <- getArgs
+    arr <- newArray (1,10) 37
     putStrLn("Hello, "++ args !! ))
 
