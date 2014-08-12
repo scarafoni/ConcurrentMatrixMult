872bd4a16310ab384e84a9c2879aed8eea67a874
scarafoni <dan@scarafoni.com>
2014-08-11 21:45:29 -0400

    fruitless attempt to get arrays to work

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,8 @@
 module Main where
 import System.Environment
-import Data.Array.IO
-
-add x y = do
-    x + y
 
 main :: IO ()
 main = do
-    args <- getArgs
-    arr <- newArray (1,10) 37
-    putStrLn("now to print the array"++newArray)
-    putStrLn("Hello, "++ args !! ))
+    arr <- range(0,10)
+    putStrLn("done")
 
