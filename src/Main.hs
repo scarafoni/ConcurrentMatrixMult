6a9ccb2db166f24256b95080d289236903ecbb27
scarafoni <dan@scarafoni.com>
2014-08-14 19:59:17 -0400

    working stuff...again

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,7 @@
 module Main where
-import System.Environment
 import GHC.TypeLits
-import Numeric.LinearAlgebra.HMatrix at LA
-
-addd :: Int -> Int -> Int
-addd x y = x + y
+import Numeric.LinearAlgebra.HMatrix
 
 main :: IO ()
 main = do
-    addd 1 1
-    putStrLn("done")
+    putStrLn "Hello, World!"
