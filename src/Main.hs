75e421df29b4d6e598d7bcb8a39cb01904ecfef1
scarafoni <dan@scarafoni.com>
2014-08-13 21:20:53 -0400

    sort of working function call

--- src/Main.hs
+++ src/Main.hs
@@ -3,11 +3,10 @@ import System.Environment
 import GHC.TypeLits
 import Numeric.LinearAlgebra.HMatrix at LA
 
+addd :: Int -> Int -> Int
+addd x y = x + y
+
 main :: IO ()
 main = do
+    addd 1 1
     putStrLn("done")
-
-add :: Int -> Int -> Int
-add x y = 
-    x + y
-
