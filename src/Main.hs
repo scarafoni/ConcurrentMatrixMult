280490f6087720fd8dc35d6c161fddd1a7800f79
scarafoni <dan@scarafoni.com>
2014-08-14 20:11:23 -0400

    function call and type cast

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,12 @@
 module Main where
 import GHC.TypeLits
-import Numeric.LinearAlgebra.HMatrix
+import Numeric.LinearAlgebra.HMatrix as MAT
+
+add :: Int -> Int -> Int
+add x y = 
+    x + y
 
 main :: IO ()
 main = do
-    putStrLn "Hello, World!"
+    let z = add 1 1
+    putStrLn $show z
