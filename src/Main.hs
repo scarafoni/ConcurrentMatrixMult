4269a673504acc0385cc36101455b766d94f83fc
Dan Scarafoni <dan@scarafoni.com>
2014-08-16 19:47:41 -0400

    commented out libs I can't import

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,17 @@
 module Main where
+{-
 import GHC.TypeLits
 import Numeric.LinearAlgebra.HMatrix as MAT
 import Control.Parallel
 
 newMat () =
     matrix 2 [1..9] * ident 2
+-}
 
 matMult m1 m2 = 
     m1
 
 main :: IO ()
 main = do
-    let x = newMat ()
-    let y = x ! 0 ! 0
+        
     putStrLn (show y)
