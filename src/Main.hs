f5385e1d6faae93f61a5ed4d345b74c8e94f7db0
scarafoni <dan@scarafoni.com>
2014-08-16 23:35:47 -0400

    parellel multiply working

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 import System.Random
+import Control.Parallel (par,pseq)
 {-
 import GHC.TypeLits
 import Numeric.LinearAlgebra.HMatrix as MAT
@@ -9,6 +10,8 @@ newMat () =
     matrix 2 [1..9] * ident 2
 -}
 
+thrd (_,_,x,_) = x
+frth (_,_,_,x) = x
 
 makeRandomRow rng = do
     let ns = randoms rng :: [Int]
@@ -25,17 +28,20 @@ cell21 a b =
     (a!!1!!0 * b!!0!!0) + (a!!1!!1 * b!!1!!0)
 cell22 a b = 
     (a!!1!!0 * b!!1!!0) + (a!!1!!1 * b!!1!!1)
+
 multMatsNorm a b = 
     [[cell11 a b, cell12 a b],
      [cell21 a b, cell22 a b]]
 
-multMatsPar= 
-    1
+multMatsPar a b = 
+    x11 `par` x12 `par` x21 `par` x22 `pseq` [[x11,x12],[x21,x22]] where
+        x11 =  cell11 a b
+        x12 =  cell12 a b
+        x21 =  cell21 a b
+        x22 =  cell22 a b
 
-main :: IO ()
-main = do        
+main = do
     rng <- newStdGen
     let x = makeRandomMat rng
     let y = makeRandomMat rng
-    let xy = multMatsNorm x y
-    putStrLn (show xy)
+    let z = multMatsNorm x y
